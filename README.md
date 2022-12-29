553416f5904494bf7d3266450c39e00744db79af
Nathanael Liechti <technat@technat.ch>
2022-12-29 16:46:40 +0100

    feat: add second config with cilium overlay networking

--- README.md
+++ README.md
@@ -4,18 +4,15 @@ Terraform code to stage AWS EKS cluster
 
 ## Usage
 
-This usage assumes:
+- Fork the repo
+- Edit the [locals.tf](./locals.tf) file to your likings
+- Create a new TF workspace in an organization of your choice
+  - set the following environment variables in the workspace:
+    - `AWS_ACCESS_KEY_ID`
+    - `AWS_SECRET_ACCESS_KEY`
+  - make sure to use VCS driven workflow and point to the folder of the configuration you want to use
 
-- that you want to use Terraform Cloud
-
-1. Fork the repo
-2. Edit the [locals.tf](./locals.tf) file to your likings
-3. Create a new TF workspace in an organization of your choice, set the following environment variables in the workspace:
-
-- `AWS_ACCESS_KEY_ID`
-- `AWS_SECRET_ACCESS_KEY`
-
-4. Let the workspace run it's first plan
+- Let the workspace run it's first plan
 
 ## Notes
 
---
+++ cilium_overlay/eks.tf
@@ -0,0 +1,125 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name                   = local.name
+  cluster_version                = local.cluster_version
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  manage_aws_auth_configmap = true
+
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = local.instance_types
+    iam_role_attach_cni_policy = true
+  }
+
+  eks_managed_node_groups = {
+    # Node group with sensible defaults, add more node groups if you want to try stuff
+    standard_node_group = {
+      name = "standard"
+      use_name_prefix = true
+      subnet_ids = module.vpc.private_subnets
+
+      min_size     = 1
+      max_size     = 5
+      desired_size = 1
+
+      ami_id                     = data.aws_ami.eks_default.image_id
+      enable_bootstrap_user_data = true
+      bootstrap_extra_args       = "--container-runtime containerd"
+
+      pre_bootstrap_user_data = <<-EOT
+        export CONTAINER_RUNTIME="containerd"
+      EOT
+
+      capacity_type        = "SPOT"
+      force_update_version = true
+      instance_types       = local.instance_types
+      labels = {}
+
+      taints = [
+        # will be removed by cilium once initalized
+        {
+          key = "node.cilium.io/agent-not-ready"
+          value = "true"
+          effect = "NO_EXECUTE"
+        }
+      ]
+
+      update_config = {
+        max_unavailable_percentage = 33 # or set `max_unavailable`
+      }
+
+      description = "Node group with sensible defaults, add more node groups if you want to try stuff"
+
+      ebs_optimized           = true
+      disable_api_termination = false
+      enable_monitoring       = true
+
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size           = 30
+            volume_type           = "gp3"
+            delete_on_termination = true
+          }
+        }
+      }
+
+      metadata_options = {
+        http_endpoint               = "enabled"
+        http_tokens                 = "required"
+        http_put_response_hop_limit = 2
+        instance_metadata_tags      = "disabled"
+      }
+
+      create_iam_role          = true
+      iam_role_name            = "eks-managed-node-group-standard"
+      iam_role_use_name_prefix = false
+      iam_role_description     = "EKS managed node group standard role"
+      iam_role_tags = {
+        Purpose = "Protector of the kubelet"
+      }
+      iam_role_additional_policies = {
+        AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+      }
+
+      tags = local.tags
+    }
+  }
+
+  tags = local.tags
+}
+
+module "vpc_cni_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name_prefix      = "VPC-CNI-IRSA"
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv4   = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
+    }
+  }
+
+  tags = local.tags
+}
